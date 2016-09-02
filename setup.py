668946f9edd3dddd676d2a002640683d9f8cbc07
Kinman Covey <kcovey@murraystate.edu>
2016-09-02 11:25:10 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="webbridge",
+    packages=["webbridge"],
+    version="0.1",
+    description="A bridge between pywebview and your web application.",
+    author="Kinman Covey",
+    author_email="kinmancovey@yahoo.com",
+    url="https://github.com/KinmanCovey/webbridge",
+    download_url="https://github.com/KinmanCovey/webbridge/tarball/0.1",
+    keywords=["bridge", "html", "gui", "web", "app"]
+)
